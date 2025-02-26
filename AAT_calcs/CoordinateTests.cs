ff552287b4804ee2c97b1d6cc3c4bddaf2f23174
throwawayexperiments <72674826+throwawayexperiments@users.noreply.github.com>
2025-02-26 02:36:16 -0500

    Calculations + Testing

---
+++ AAT_calcs/CoordinateTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+
+namespace AAT_calcs;
+
+[TestFixture]
+public class CoordinateTests
+{
+    [Test]
+    [TestCase(0, 0, 0, 6378137.0, 0.0, 0.0)]
+    [TestCase(90, 90, 0, 0.0, 0.0, 6356752.3)]
+    [TestCase(-90, -0, 0, 0.0, 0.0, -6356752.3)]
+    [TestCase(35.6895, 139.6917, 10, -3954850.4855, 3354942.189, 3700270.608)]
+    [TestCase(45.555, 100.0909, 30, -783853.6869, 4404581.265, 4530773.044)]
+    [TestCase(45.555, 100.0909, 20, -783852.4600, 4404574.371, 4530765.904)]
+
+    public void ConvertToECEF_Test(double latitude, double longitude, double altitude, double expectedX,
+        double expectedY, double expectedZ)
+    {
+        var (x, y, z) = Mapping.MapToXyzTuple(latitude, longitude, altitude);
+        Assert.That(x, Is.EqualTo(expectedX).Within(1E-1));
+        Assert.That(y, Is.EqualTo(expectedY).Within(1E-1));
+        Assert.That(z, Is.EqualTo(expectedZ).Within(1E-1));
+        
+    }
+
+    [Test]
+    [TestCase(-783853.6869, 4404581.265, 4530773.044, -783852.4600, 4404574.371, 4530765.904, 10)]
+    [TestCase(-783853.6869, 4404581.265, 4530773.044, -3954850.4855, 3354942.189, 3700270.608, 3441903.1336)]
+    [TestCase(0, 0, 0, 6378137.0, 0, 0, 6378137.0)]
+    [TestCase(0, 0, 0, 0, 0, 6356752.3, 6356752.3)]
+    [TestCase(0, 0, 0, 0, 0, -6356752.3, 6356752.3)]
+    public void CartesianDistance_Test(double ax, double ay, double az, double bx, double by, double bz, double expectedDist)
+    {
+        double dist = Mapping.CartesianCoordinateDistance(ax, ay, az, bx, by, bz);
+        Assert.That(dist, Is.EqualTo(expectedDist).Within(1E-1));
+    }
+
+    [Test]
+    [TestCase(35.6895, 139.6917, 10, 45.555, 100.0909, 30, 3441903.1336)]
+    [TestCase(90, 90, 0, -90, 90, 0, 12713504.6)]
+    public void GpsCoordinateDistance_Test(double aLat, double aLon, double aH, double bLat, double bLon, double bH,
+        double expectedDist)
+    {
+        double dist = Mapping.GpsCoordinateDistance(aLat, aLon, aH, bLat, bLon, bH);
+        Assert.That(dist, Is.EqualTo(expectedDist).Within(1E-1));
+    }
+}
---
+++ AAT_calcs/Mapping.cs
@@ -0,0 +1,51 @@
+﻿namespace AAT_calcs;
+
+// x = R * cos(lat) * cos(lon)
+//
+// y = R * cos(lat) * sin(lon)
+//
+// z = R *sin(lat)
+// a ( equatorial radius ) = 6378137.0 m
+// b ( polar radius ) = 6356752.3 m
+// e2 ( eccentricity ) = 1 - (b^2/a^2) => f = 1 - (b / a)
+
+public static class Mapping
+{
+    private const double a = 6378137.0;
+    private const double b = 6356752.3;
+    private const double e = 0.08181921804834474711755146934483;
+
+    private static double PrimeVerticalRadius(double latRadians)
+    {
+        // N(∅) = a / sqrt(1 - ((e^2) / (1 + cot^2(∅)))
+        double denominator =Math.Sqrt(1 - Math.Pow(e, 2) * Math.Pow(Math.Sin(latRadians), 2));
+        return a / denominator;
+    }
+
+    public static (double X, double Y, double Z) MapToXyzTuple(double pLat, double pLon, double heightMeters = 0)
+    {
+        // ∅ = latitude
+        // λ = longitude
+        double latRadians = pLat * Math.PI / 180.0;
+        double lonRadians = pLon * Math.PI / 180.0;
+        double pvr = PrimeVerticalRadius(latRadians);
+        double x = (pvr + heightMeters)*Math.Cos(latRadians) * Math.Cos(lonRadians);
+        double y = (pvr + heightMeters)*Math.Cos(latRadians) * Math.Sin(lonRadians);
+        double z = (((1 - Math.Pow(e,2)) * pvr) + heightMeters) * Math.Sin(latRadians);
+        return (x, y, z);
+    }
+
+    public static double CartesianCoordinateDistance(double ax, double ay, double az, double bx, double by, double bz)
+    {
+        return Math.Sqrt(Math.Pow(ax-bx, 2) + Math.Pow(ay-by, 2) + Math.Pow(az-bz, 2));
+    }
+
+    public static double GpsCoordinateDistance(double aLat, double aLon, double aH, double bLat, double bLon,
+        double bH)
+    {
+        var a = MapToXyzTuple(aLat, aLon, aH);
+        var b = MapToXyzTuple(bLat, bLon, bH);
+        return CartesianCoordinateDistance(a.X, a.Y, a.Z, b.X, b.Y, b.Z);
+    }
+    
+}
